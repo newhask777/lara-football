0e7fdb1226ffbcbadc3fcf1ba4ef1c87292f6c2e
newhask777 <newhaskellisp@gmail.com>
2024-01-02 16:16:11 +0300

    last

---
+++ app/Http/Controllers/Front/Predictions/PredictionsByCountryToday.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Front\Predictions;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class PredictionsByCountryToday extends Controller
+{
+    public function __invoke($country)
+    {
+        $games = DB::table('predictions')
+            ->select('*')
+            ->where('competition_cluster', $country)
+            ->get();
+
+        $tournaments = DB::table('predictions')
+            ->select('competition_cluster','competition_name', 'federation')
+            ->distinct('competition_cluster')
+            ->distinct('competition_name')
+            ->distinct('federation')
+            ->get();
+
+        $federations = DB::table('predictions')
+            ->select('federation')
+            ->distinct('federation')
+            ->get();
+
+        $countries = DB::table('predictions')
+            ->select('competition_cluster')
+            ->distinct('competition_cluster')
+            ->get();
+
+//        dd($games);
+
+        $temp = 'predictions';
+        $type = 'country-today';
+
+        $currentDate = date('Y-m-d');
+
+
+        return view('predictions.today', [
+            'games' => $games,
+            'temp' => $temp,
+            'type' => $type,
+            'tournaments' => $tournaments,
+            'federations' => $federations,
+            'country' => $country,
+            'countries' => $countries,
+            'currentDate' => $currentDate,
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Front/Predictions/PredictionsByFederationToday.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Front\Predictions;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class PredictionsByFederationToday extends Controller
+{
+    public function __invoke($federation)
+    {
+        $games = DB::table('predictions')
+            ->select('*')
+            ->where('federation', $federation)
+            ->get();
+
+        $tournaments = DB::table('predictions')
+            ->select('competition_cluster','competition_name', 'federation')
+            ->distinct('competition_cluster')
+            ->distinct('competition_name')
+            ->distinct('federation')
+            ->get();
+
+        $federations = DB::table('predictions')
+            ->select('federation')
+            ->distinct('federation')
+            ->get();
+
+        $countries = DB::table('predictions')
+            ->select('competition_cluster')
+            ->distinct('competition_cluster')
+            ->get();
+
+//        dd($games);
+
+        $temp = 'predictions';
+        $type = 'federation-today';
+
+        $currentDate = date('Y-m-d');
+
+
+        return view('predictions.today', [
+            'games' => $games,
+            'temp' => $temp,
+            'type' => $type,
+            'tournaments' => $tournaments,
+            'federations' => $federations,
+            'federation' => $federation,
+            'countries' => $countries,
+            'currentDate' => $currentDate,
+        ]);
+    }
+}
